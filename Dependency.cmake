c5bdede79b76c1c23170dd3d5e542d4c5de7ba20
Rinthel <rinthel@gmail.com>
2022-08-06 09:57:22 +0900

    update dependencies / add glfw3 window

--- Dependency.cmake
+++ Dependency.cmake
@@ -8,7 +8,8 @@ endif()
 
 set(DEPENDENCY_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
 set(DEPENDENCY_INCLUDE_DIR ${DEPENDENCY_INSTALL_DIR}/include)
-set(DEPENDENCY_LIBS ${LINK_DIR_OPTION}${DEPENDENCY_INSTALL_DIR}/lib)
+set(DEPENDENCY_LIB_DIR ${DEPENDENCY_INSTALL_DIR}/lib)
+# set(DEPENDENCY_LIBS ${LINK_DIR_OPTION}${DEPENDENCY_LIB_DIR})
 
 # spdlog: fast logger library
 ExternalProject_Add(
@@ -22,7 +23,12 @@ ExternalProject_Add(
     TEST_COMMAND ""
 )
 set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep-spdlog)
-set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+# if (GENERATOR_IS_MULTI_CONFIG)
+if (WIN32)
+    set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} spdlog$<$<CONFIG:Debug>:d>)
+else()
+    set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} spdlog)
+endif()
 
 # clipp: header-only good c++ argument parser
 ExternalProject_Add(
@@ -62,7 +68,7 @@ set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep-json)
 ExternalProject_Add(
     dep_magic_enum
     GIT_REPOSITORY "https://github.com/Neargye/magic_enum.git"
-    GIT_TAG "v0.6.6"
+    GIT_TAG "v0.7.3"
     GIT_SHALLOW 1
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
@@ -89,3 +95,80 @@ ExternalProject_Add(
     TEST_COMMAND ""
     )
 set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep_filesystem)
+
+# glfw
+ExternalProject_Add(
+    dep_glfw
+    GIT_REPOSITORY "https://github.com/glfw/glfw.git"
+    GIT_TAG "3.3-stable"
+    GIT_SHALLOW 1
+    UPDATE_COMMAND ""
+    PATCH_COMMAND ""
+    CMAKE_ARGS
+        -DCMAKE_INSTALL_PREFIX=${DEPENDENCY_INSTALL_DIR}
+        -DGLFW_BUILD_EXAMPLES=OFF
+        -DGLFW_BUILD_TESTS=OFF
+        -DGLFW_BUILD_DOCS=OFF
+    TEST_COMMAND ""
+    )
+set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep_glfw)
+set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} glfw3)
+
+## additional dependencies, for graphics programming
+
+# # stb: the king of header-only libraries
+# ExternalProject_Add(
+#     dep-stb
+#     GIT_REPOSITORY "https://github.com/nothings/stb.git"
+#     GIT_SHALLOW 1
+#     UPDATE_COMMAND ""
+#     PATCH_COMMAND ""
+#     CONFIGURE_COMMAND ""
+#     BUILD_COMMAND ""
+#     INSTALL_COMMAND
+#         ${CMAKE_COMMAND} -E make_directory ${DEPENDENCY_INSTALL_DIR}/include &&
+#         ${CMAKE_COMMAND} -E copy
+#             ${CMAKE_CURRENT_BINARY_DIR}/dep-stb-prefix/src/dep-stb/stb_image.h ${DEPENDENCY_INSTALL_DIR}/include
+#             ${CMAKE_CURRENT_BINARY_DIR}/dep-stb-prefix/src/dep-stb/stb_truetype.h ${DEPENDENCY_INSTALL_DIR}/include
+#     TEST_COMMAND ""
+#     )
+# set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep-stb)
+
+# # linalg.h: single header, public domain, short vector math library for c++
+# ExternalProject_Add(
+#     dep-linalg
+#     GIT_REPOSITORY "https://github.com/sgorsten/linalg.git"
+#     GIT_TAG main
+#     GIT_SHALLOW 1
+#     UPDATE_COMMAND ""
+#     PATCH_COMMAND ""
+#     CONFIGURE_COMMAND ""
+#     BUILD_COMMAND ""
+#     INSTALL_COMMAND
+#         ${CMAKE_COMMAND} -E make_directory ${DEPENDENCY_INSTALL_DIR}/include &&
+#         ${CMAKE_COMMAND} -E copy
+#             ${CMAKE_CURRENT_BINARY_DIR}/dep-linalg-prefix/src/dep-linalg/linalg.h ${DEPENDENCY_INSTALL_DIR}/include
+#     TEST_COMMAND ""
+#     )
+# set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep-linalg)
+
+# # imgui: best immediate-mode gui library for all graphics applications
+# ExternalProject_Add(
+#     dep-imgui
+#     GIT_REPOSITORY "https://github.com/ocornut/imgui.git"
+#     GIT_TAG v1.85
+#     GIT_SHALLOW 1
+#     UPDATE_COMMAND ""
+#     PATCH_COMMAND
+#         ${CMAKE_COMMAND} -E copy
+#             ${CMAKE_CURRENT_SOURCE_DIR}/cmake/imgui.cmake
+#             ${CMAKE_CURRENT_BINARY_DIR}/dep-imgui-prefix/src/dep-imgui/CMakeLists.txt
+#     CMAKE_ARGS
+#         -DCMAKE_INSTALL_PREFIX=${DEPENDENCY_INSTALL_DIR}
+#         -DIMGUI_GLFW_INCLUDE_DIR=${DEPENDENCY_INCLUDE_DIR}
+#         -DIMGUI_VULKAN_INCLUDE_DIR=${Vulkan_INCLUDE_DIR}
+#     TEST_COMMAND ""
+#     )
+# add_dependencies(dep-imgui dep-glfw)
+# set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep-imgui)
+# set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} imgui)
--- src/main.cpp
+++ src/main.cpp
@@ -7,8 +7,29 @@
 #include <ghc/filesystem.hpp>
 #include <magic_enum.hpp>
 #include <clipp.h>
+#include <GLFW/glfw3.h>
 
 int main(int argc, const char** argv) {
-    SPDLOG_INFO("Hello, world!");
+    SPDLOG_INFO("Initialize glfw");
+    if (!glfwInit()) {
+        const char* description = nullptr;
+        glfwGetError(&description);
+        SPDLOG_ERROR("failed to initialize glfw: {}", description);
+        return -1;
+    }
+
+    auto window = glfwCreateWindow(640, 480, "First window", nullptr, nullptr);
+    if (!window) {
+        SPDLOG_ERROR("failed to create glfw window");
+        glfwTerminate();
+        return -1;
+    }
+
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
     return 0;
 }
+
