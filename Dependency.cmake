a8c488f64af4bce0014bc3a72c37b1e2d7146535
Rinthel Kwon <rinthel@gmail.com>
2025-04-13 14:11:48 +0900

    update dependency to latest version / separate dependency src/install dirs

--- Dependency.cmake
+++ Dependency.cmake
@@ -6,7 +6,8 @@ elseif()
     set(LINK_DIR_OPTION -L)
 endif()
 
-set(DEPENDENCY_INSTALL_DIR ${PROJECT_BINARY_DIR}/install)
+set(DEPENDENCY_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/dependency)
+set(DEPENDENCY_INSTALL_DIR ${DEPENDENCY_SRC_DIR}/install)
 set(DEPENDENCY_INCLUDE_DIR ${DEPENDENCY_INSTALL_DIR}/include)
 set(DEPENDENCY_LIB_DIR ${DEPENDENCY_INSTALL_DIR}/lib)
 # set(DEPENDENCY_LIBS ${LINK_DIR_OPTION}${DEPENDENCY_LIB_DIR})
@@ -17,6 +18,7 @@ ExternalProject_Add(
     GIT_REPOSITORY "https://github.com/gabime/spdlog.git"
     GIT_TAG "v1.x"
     GIT_SHALLOW 1
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/dependency/src/spdlog
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
     CMAKE_ARGS -DCMAKE_INSTALL_PREFIX=${DEPENDENCY_INSTALL_DIR}
@@ -34,8 +36,9 @@ endif()
 ExternalProject_Add(
     dep-json
     GIT_REPOSITORY "https://github.com/nlohmann/json.git"
-    GIT_TAG "v3.11.3"
+    GIT_TAG "v3.12.0"
     GIT_SHALLOW 1
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/dependency/src/json
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
     CMAKE_ARGS
@@ -49,8 +52,9 @@ set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep-json)
 ExternalProject_Add(
     dep_magic_enum
     GIT_REPOSITORY "https://github.com/Neargye/magic_enum.git"
-    GIT_TAG "v0.9.6"
+    GIT_TAG "v0.9.7"
     GIT_SHALLOW 1
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/dependency/src/magic_enum
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
     CMAKE_ARGS
@@ -61,28 +65,13 @@ ExternalProject_Add(
     )
 set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep_magic_enum)
 
-# filesystem: c++14-based filesystem that implements c++17 filesystem
-ExternalProject_Add(
-    dep_filesystem
-    GIT_REPOSITORY "https://github.com/gulrak/filesystem.git"
-    GIT_TAG "v1.5.14"
-    GIT_SHALLOW 1
-    UPDATE_COMMAND ""
-    PATCH_COMMAND ""
-    CMAKE_ARGS
-        -DCMAKE_INSTALL_PREFIX=${DEPENDENCY_INSTALL_DIR}
-        -DGHC_FILESYSTEM_BUILD_TESTING=OFF
-        -DGHC_FILESYSTEM_BUILD_EXAMPLES=OFF
-    TEST_COMMAND ""
-    )
-set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep_filesystem)
-
-# sdl2
+# sdl3
 ExternalProject_Add(
     dep_sdl
     GIT_REPOSITORY "https://github.com/libsdl-org/SDL.git"
-    GIT_TAG "release-2.30.5"
+    GIT_TAG "release-3.2.10"
     GIT_SHALLOW 1
+    SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/dependency/src/sdl
     UPDATE_COMMAND ""
     PATCH_COMMAND ""
     CMAKE_ARGS
@@ -92,9 +81,9 @@ ExternalProject_Add(
     )
 set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep_sdl)
 if (MSVC)
-    set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} SDL2$<$<CONFIG:Debug>:d>)
+    set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} SDL3$<$<CONFIG:Debug>:d>)
 else()
-    set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} SDL2)
+    set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} SDL3)
 endif()
 
 ## additional dependencies, for graphics programming
@@ -192,3 +181,20 @@ endif()
 #     )
 # set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep_glfw)
 # set(DEPENDENCY_LIBS ${DEPENDENCY_LIBS} glfw3)
+
+# # filesystem: c++14-based filesystem that implements c++17 filesystem
+# ExternalProject_Add(
+#     dep_filesystem
+#     GIT_REPOSITORY "https://github.com/gulrak/filesystem.git"
+#     GIT_TAG "v1.5.14"
+#     GIT_SHALLOW 1
+#     UPDATE_COMMAND ""
+#     PATCH_COMMAND ""
+#     CMAKE_ARGS
+#         -DCMAKE_INSTALL_PREFIX=${DEPENDENCY_INSTALL_DIR}
+#         -DGHC_FILESYSTEM_BUILD_TESTING=OFF
+#         -DGHC_FILESYSTEM_BUILD_EXAMPLES=OFF
+#     TEST_COMMAND ""
+#     )
+# set(DEPENDENCY_LIST ${DEPENDENCY_LIST} dep_filesystem)
+
--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,13 @@
 #include <spdlog/spdlog.h>
 
 #define SDL_MAIN_HANDLED
-#include <SDL2/SDL.h>
+#include <SDL3/SDL.h>
 
 int main(int argc, const char** argv) {
-    SPDLOG_INFO("Initialize SDL2");
+    SPDLOG_INFO("Initialize SDL3");
 
     SDL_Init(SDL_INIT_VIDEO);
-    auto window = SDL_CreateWindow("CPP template", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
-        1280, 720, 0);
+    auto window = SDL_CreateWindow("CPP template", 1280, 720, 0);
 
     SDL_Event e;
     bool bQuit = false;
@@ -21,7 +20,7 @@ int main(int argc, const char** argv) {
         // Handle events on queue
         while (SDL_PollEvent(&e) != 0) {
             // close the window when user alt-f4s or clicks the X button
-            if (e.type == SDL_QUIT)
+            if (e.type == SDL_EVENT_QUIT)
                 bQuit = true;
         }
     }
